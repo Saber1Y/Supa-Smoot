c263e968be60ae133319a1dfa0b1a6b4dbcc8dcb
Saber <ayodejiakintobi1@gmail.com>
2025-07-06 11:52:36 +0100

    feat: fetched data from supabase

--- src/config/SupabaseClient.js
+++ src/config/SupabaseClient.js
@@ -6,3 +6,5 @@ const supabaseKey = process.env.REACT_APP_SUPABASE_KEY
 
 
 const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react";
+import supabase from "../config/SupabaseClient";
+
 const Home = () => {
+  const [smoothy, setSmoothy] = useState(null);
+  const [error, setError] = useState();
+
+
+  useEffect(() => {
+    const fetchSmooties = async() => {
+      const { data, error } = await supabase
+      .from("Supa Smot")
+      .select()
+
+      if (error) {
+        setError("Could not fetch Smooties sorry");
+        console.log("error", error);
+        setSmoothy(null)
+      }
+
+      if (data) {
+        setError(null)
+        setSmoothy(data);
+      }
+    } 
+
+    fetchSmooties(); //call function to dispplay
+  }, [])
+  
   return (
     <div className="page home">
       <h2>Home</h2>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
